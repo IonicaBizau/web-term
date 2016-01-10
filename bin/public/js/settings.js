840974ab3c251590537d9c3a9e4af0298ec60ca1
Ionică Bizău <bizauionica@gmail.com>
2016-01-10 17:31:09 +0200

    Fluid container

--- bin/public/js/settings.js
+++ bin/public/js/settings.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
     $.getJSON("/api/settings/get", function (json) {
         $form.trigger("serializer:fill", json);
     });
+    var $submitButton = $(".btn-primary");
+    $submitButton.attr("disabled", "disabled");
+    $form.on("change", function () {
+        $submitButton.removeAttr("disabled", "disabled");
+    });
     $form.on("serializer:data", function (ev, data) {
         $.ajax({
             url: "/api/settings/save"
@@ -10,8 +15,12 @@ $(document).ready(function () {
           , dataType: "json"
           , contentType : 'application/json'
           , type: "POST"
-        }, function () {
-
+          , success: function () {
+                $submitButton.attr("disabled", "disabled");
+            }
+          , error: function (xhr, code, error) {
+                alert(code + ":" + error);
+            }
         });
     });
 });
